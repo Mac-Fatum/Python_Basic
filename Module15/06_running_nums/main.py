09a7924cf8e8bade8f33d9783e15dabc3df8f095
User <romanzheltov1@gmail.com>
2025-01-24 18:17:27 +0200

    Дз до 6 задания

--- Module15/06_running_nums/main.py
+++ Module15/06_running_nums/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+n = int(input('Количество элементов в списке? '))
+shift = int(input('Сдвиг: '))
+n_list = []
+new_list = []
+
+for i in range(n):
+    item = int(input(f'Введите {i + 1} элемент списка: '))
+    n_list.append(item)
+
+new_place = 0
+for index, item in enumerate(new_list):
+    for i in range(index):
+        i += 1
+        if i > n:
+            i = 0
+    new_list.append()
+
+print(new_list)
