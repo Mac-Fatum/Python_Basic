aabb3834f8ade80c6a2b88cb87cf397048418ec1
User <romanzheltov1@gmail.com>
2025-02-01 13:33:35 +0200

    rr

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,32 @@
-print('hEllo')
+films = [
+
+    'Крепкий орешек', 'Назад в будущее', 'Таксист',
+
+    'Леон', 'Богемская рапсодия', 'Город грехов',
+
+    'Мементо', 'Отступники', 'Деревня',
+
+    'Проклятый остров', 'Начало', 'Матрица'
+
+]
+
+my_films = []
+
+while True:
+    print(f'\nВаш текущий топ фильмов: {my_films}')
+    film = input('Название фильма: ')
+    if film in films:
+        print('Команды: добавить, вставить, удалить')
+        command = input('Введите команду: ')
+        if command == 'добавить':
+            if film not in my_films:
+                my_films.append(film)
+            else:
+                print('Фильм уже в вашем каталоге.')
+        if command == 'вставить':
+            index = int(input('На какое место? '))
+            my_films.insert(index - 1, film)
+        if command == 'удалить':
+            my_films.remove(film)
+    else:
+        print('Данного фильма на нашем сайте')
--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('Hello World!')
+goods = [["яблоки", 50], ["апельсины", 190], ["груши", 100], ["нектарины", 200], ["бананы", 77]]
+print(f'Текущий ассортимент - {goods}')
+
+new_fruit = input('\nНовый фрукт: ')
+price = int(input('Цена: '))
+
+goods.append([new_fruit, price])
+print(f'\nНовый ассортимент - {goods}')
+
+for i_list in goods:
+    i_list[1] = round(i_list[1] * 1.08, 2)
+
+print(f'\nНовый ассортимент с увеличенной ценой - {goods}')
